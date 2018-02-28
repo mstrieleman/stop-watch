1c956431629e8f7d0cb2cbde8405c3f2485caae2
mstrieleman <mstrieleman@gmail.com>
2018-02-28 13:26:42 -0800

    Revised formatting, moved event listener into start.js, and swapped innerHTML for textContent on line 19.

--- start.js
+++ start.js
@@ -1,24 +1,32 @@
-var startTime = null
+var startTime = null;
 
 function start() {
-  var started = null
-    if (startTime === null) {
-        startTime = new Date()
-        started = setInterval(renderClock, 10)
-    }
+  var started = null;
+  if (startTime === null) {
+    startTime = new Date();
+    started = setInterval(renderClock, 10);
+  }
 }
 
-function renderClock(){
-  var currentTime = new Date()
-  var timeElapsed = new Date(currentTime - startTime)
-  var hours = timeElapsed.getUTCHours()
-  var minutes = timeElapsed.getUTCMinutes()
-  var seconds = timeElapsed.getUTCSeconds()
-  var ms = timeElapsed.getUTCMilliseconds()
+var $startButton = document.querySelector("#start-button");
+$startButton.addEventListener("click", function() {
+  start();
+});
 
-  document.getElementById("stop-watch").innerHTML =
-  (hours > 9 ? hours : "0" + hours) + ":" +
-  (minutes > 9 ? minutes : "0" + minutes) + ":" +
-  (seconds > 9 ? seconds : "0" + seconds) + ":" +
-  (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms)
+function renderClock() {
+  var currentTime = new Date();
+  var timeElapsed = new Date(currentTime - startTime);
+  var hours = timeElapsed.getUTCHours();
+  var minutes = timeElapsed.getUTCMinutes();
+  var seconds = timeElapsed.getUTCSeconds();
+  var ms = timeElapsed.getUTCMilliseconds();
+
+  document.getElementById("stop-watch").textContent =
+    (hours > 9 ? hours : "0" + hours) +
+    ":" +
+    (minutes > 9 ? minutes : "0" + minutes) +
+    ":" +
+    (seconds > 9 ? seconds : "0" + seconds) +
+    ":" +
+    (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
 }
